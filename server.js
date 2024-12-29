d06ab85ee40afd39b7945910aecf0fc9c0ad82f2
vashu-aggarwal <vashuaggarwal05@gmail.com>
2024-12-29 20:21:25 +0530

    Day 19

--- server.js
+++ server.js
@@ -1,41 +1,46 @@
-const express = require('express');
-const dotenv= require('dotenv');
-const cors= require('cors');
-const morgan = require('morgan');
-const colors= require('colors');
-const connectDB = require('./config/db');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const morgan = require("morgan");
+const colors = require("colors");
+const connectDB = require("./config/db");
+const path = require("path");
 
 dotenv.config();
-console.log("MONGO_URL:", process.env.MONGO_URL); // This should output your MongoDB URL
-//connecting to the database
+if (process.env.NODE_ENV !== "production") {
+  console.log("MONGO_URL:", process.env.MONGO_URL);
+}
 connectDB();
 
 //res object
-const app=express();
+const app = express();
 
 // middleware
 app.use(express.json());
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-// 1 test Routes 
-app.use('/api/v1/test',require('./routes/testRoutes'));
+// 1 test Routes
+app.use("/api/v1/test", require("./routes/testRoutes"));
+app.use("/api/v1/auth", require("./routes/authRoutes"));
+app.use("/api/v1/inventory", require("./routes/inventoryRoutes"));
+app.use("/api/v1/analytics", require("./routes/analyticsRoutes"));
+app.use("/api/v1/admin", require("./routes/adminRoutes"));
 
-app.use('/api/v1/auth',require('./routes/authRoutes'));
+//  STATIC FOLDERS
+app.use(express.static(path.join(__dirname, "./client/build")));
 
-app.use('/api/v1/inventory',require('./routes/inventoryRoutes'));
-
-app.use('/api/v1/analytics',require('./routes/analyticsRoutes'));
-
-
-app.use('/api/v1/admin',require('./routes/adminRoutes'));
-
+//STATIC ROUTES
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 //PORT
 
-const PORT=process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
-app.listen(PORT,()=>{
-    console.log(`Node server Running in ${process.env.DEV_MODE} - mode on Port ${process.env.PORT}`
-        .bgBlue.white
-    );
+app.listen(PORT, () => {
+  console.log(
+    `Node server Running in ${process.env.DEV_MODE} - mode on Port ${process.env.PORT}`
+      .bgBlue.white
+  );
 });
